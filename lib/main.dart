924c4d7cc5bde68ec75a742ccad2eee1018bc12f
6210110334 <6210110334@psu.ac.th>
2021-08-13 17:46:15 +0700

    add service hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:exam_mid/screens/last_time_page.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
   runApp(MyApp());
 }
 
--- lib/models/last_time.dart
+++ lib/models/last_time.dart
@@ -1,6 +1,15 @@
-class LastTime {
-  String title;
-  DateTime time;
-  String mode;
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class LastTime extends HiveObject {
+  @HiveField(0)
+  late String title;
+
+  @HiveField(1)
+  late DateTime time;
+
+  @HiveField(2)
+  late String mode;
+
   LastTime(this.time, this.title, this.mode);
 }
